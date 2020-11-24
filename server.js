2750e322ad76e000543cc28a5a0673a0addaec86
sanskar <sanskar2999@gmail.com>
2020-11-24 18:33:36 +0530

    images add

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
@@ -8,7 +9,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app .use(cors());
-app.use(express.json());
+// app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false })) 
+app.use(bodyParser.json())
+app.set("view engine", "ejs"); 
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true}
