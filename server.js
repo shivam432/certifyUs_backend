9c95eda638a18b1976deef7eca065fcd038e448a
sanskar <sanskar2999@gmail.com>
2020-11-25 12:07:12 +0530

    new

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 app.set("view engine", "ejs"); 
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true}
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology: true}
     );
      
 const connection = mongoose.connection;
