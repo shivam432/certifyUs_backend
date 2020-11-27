436af29b5560c2747f279c8cc5e26773b9b300f2
sanskar <sanskar2999@gmail.com>
2020-11-27 10:14:57 +0530

    signup

---
+++ models/user.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    email:{ type: String,required:true},
+    password:{ type: String, required:true}
+});
+
+const User = mongoose.model('User',userSchema);
+
+module.exports = User;
+
