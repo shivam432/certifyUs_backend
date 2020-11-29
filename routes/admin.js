0ea4cdfdd44e2c754ca14c24c4866e82904ce971
sanskar <sanskar2999@gmail.com>
2020-11-29 17:40:38 +0530

    new routes added

--- routes/admin.js
+++ routes/admin.js
@@ -20,6 +20,13 @@ router.route('/').get((req,res) => {
      .catch(err => res.status(400).json('error ' + err)); 
 });
 
+router.route('/:email').get((req,res,next) => {
+    const id = req.params.email;
+    Admin.find({ email: id })
+     .then(templates => res.json(templates))
+     .catch(err => res.status(400).json('error ' + err)); 
+});
+
 router.post('/add', upload.single('image'), (req, res, next) => { 
    const newAdmin = new Admin({
        image:req.file.filename,
