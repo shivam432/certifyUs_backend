f93c5498a6424963c5f407dae34781f8ee88c7ed
sanskar <sanskar2999@gmail.com>
2020-12-04 00:08:26 +0530

    schema change

--- models/admin.model.js
+++ models/admin.model.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const adminSchema = new Schema({
      image:{ type:String},
      name:{type:String , required:true},
-     email:{type:String, required:true}
+     email:{type:String, required:true},
+     certificates:{type:Array},
 });
 
 const Admin = mongoose.model('Admin',adminSchema);
--- models/customers.model.js
+++ models/customers.model.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const customersSchema = new Schema({
     image:{ type:String},
     name:{type:String , required:true},
-    email:{type:String, required:true}
+    email:{type:String, required:true},
+    certificates:{type:Array},
 });
 
 const Customers = mongoose.model('Customers',customersSchema);
