b16aeb11a4597bfc66e7f7d0c0fe5739d04ada0d
shivam432 <2018cse_shivam@nie.ac.in>
2020-12-27 20:29:33 +0530

    templates add done

--- routes/templates.js
+++ routes/templates.js
@@ -1,18 +1,18 @@
  const router = require('express').Router();
-
- let Template = require('../models/templates.model');
-
+ const mongoose = require('mongoose');
+ const Template = require('../models/templates.model');
+ var Jimp = require('jimp');
 var fs = require('fs'); 
 var path = require('path'); 
 var multer = require('multer'); 
   
 var storage = multer.diskStorage({ 
-    destination: (req, file, cb) => { 
+    destination: (req, file, cb) => {
         cb(null, './uploads/') 
     }, 
-    filename: (req, file, cb) => { 
+    filename: (req, file, cb) => {
         cb(null, `${new Date().toISOString().replace(/:/g, '-')}${file.originalname}`);
-    } 
+    }
 }); 
   
 var upload = multer({ storage: storage })
@@ -23,31 +23,38 @@ var upload = multer({ storage: storage })
       .catch(err => res.status(400).json('error ' + err)); 
  });
 
-//  router.route('/add').post((req,res)=>{
-//      const url =req.body.url;
+ router.post('/add', upload.single('file') ,(req, res, next)=>{
+     console.log(req.body.name);
+     console.log('hey man');
+    Template.find({name:req.body.name})
+    .exec()
+    .then(async template=> {
+        if(template.length>1){
+            return res.status(409).json({
+                message: 'Template already exists'
+            })
+        }else{
+            const add_template= new Template({
+                path: req.file.path,
+                url:req.file.filename,
+                name:req.body.name,
+            });
+            add_template.save()
+            .then(result=>{
+                console.log(result);
+                res.status(201).json({
+                    message:'Template Created'
+                });
+            })
+            .catch(err=>{
+                res.status(500).json({
+                    error:err
+                })
+            })
+        }
+    })
+ });
 
-//      const newTemplate = new Template({url});
 
-//      newTemplate.save()
-//      .then(() => res.json('Template added !'))
-//      .catch(err => res.status(400).json('Error: ' + err));
-
-//  });
-
- // Uploading the image 
-router.post('/add', upload.single('image'), (req, res, next) => { 
-    const newTemplate = new Template({
-        path:req.file.path,
-        url:req.file.filename,
-        name:req.body.name,
-        // img: { 
-        //         data: fs.readFileSync(path.join('/home/sanskar/Desktop/DBMS_project_backend' + '/uploads/' + req.file.filename)), 
-        //         contentType: 'image/png'
-        //     } 
-    });
-    newTemplate.save()
-    .then(() => res.json('Template added !'))
-    .catch(err => res.status(400).json('Error: ' + err));
-    });
 
  module.exports = router;
--- server.js
+++ server.js
@@ -18,11 +18,10 @@ app .use(cors());
 // app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false })) 
 app.use(bodyParser.json())
-app.set("view engine", "ejs"); 
+app.set("view engine", "ejs");
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology: true, useFindAndModify: false }
-    );
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology: true, useFindAndModify: false });
      
 const connection = mongoose.connection;
  connection.once('open',()=>{
